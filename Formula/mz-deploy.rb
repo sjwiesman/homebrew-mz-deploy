4b897f0fd15179ee3a46a24432824259195fff04
Seth Wiesman <sjwiesman@gmail.com>
2026-05-03 09:57:58 -0500

    Update mz-deploy to v0.10.1

--- Formula/mz-deploy.rb
+++ Formula/mz-deploy.rb
@@ -1,26 +1,26 @@
 class MzDeploy < Formula
   desc "Deployment tool for Materialize"
   homepage "https://github.com/sjwiesman/mz-deploy"
-  version "0.10.0"
+  version "0.10.1"
   license "BSL-1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sjwiesman/mz-deploy/releases/download/v0.10.0/mz-deploy-v0.10.0-aarch64-apple-darwin.tar.gz"
-      sha256 "2de2b78fcc0f537258f84ef8d07c8dbd9731781effb2558f33f79e8541065ed9"
+      url "https://github.com/sjwiesman/mz-deploy/releases/download/v0.10.1/mz-deploy-v0.10.1-aarch64-apple-darwin.tar.gz"
+      sha256 "e1a2dc80bbdfa872990a3c20dd7b8d8e8b0c5338f5c9c1ccccbcdbba35e5585a"
     else
-      url "https://github.com/sjwiesman/mz-deploy/releases/download/v0.10.0/mz-deploy-v0.10.0-x86_64-apple-darwin.tar.gz"
-      sha256 "5ed98bc38d83842c7a823f068e7226a31260c2080bef059c46d2e63b1e7d9b74"
+      url "https://github.com/sjwiesman/mz-deploy/releases/download/v0.10.1/mz-deploy-v0.10.1-x86_64-apple-darwin.tar.gz"
+      sha256 "9419d37fabbe017bf56dfc76b23d03c0eef6e9e005479dd73a67bc6dbdca08d3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sjwiesman/mz-deploy/releases/download/v0.10.0/mz-deploy-v0.10.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "3f647ed95a84e9e827bf57737266f533b18d6e3d816063fef8e44126799b67fd"
+      url "https://github.com/sjwiesman/mz-deploy/releases/download/v0.10.1/mz-deploy-v0.10.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "6f3713166a8fa06a1796de9a1adf0735358a74c6eebcc0ea618478ef91d8e70a"
     else
-      url "https://github.com/sjwiesman/mz-deploy/releases/download/v0.10.0/mz-deploy-v0.10.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "785d374508e5f6ae5748389746553d8ee062e7e9560d0dd227ba0a2696a95a26"
+      url "https://github.com/sjwiesman/mz-deploy/releases/download/v0.10.1/mz-deploy-v0.10.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "9ebe9a1ea3fc4d5649d1f5729e4f46c5885b1cc502e1f3d77d69e1fa5b23b31c"
     end
   end
 
